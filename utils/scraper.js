d1a25844cd6feda0476858c58dcb25e8f20e0ceb
navid.mnzh <navidproject283@gmail.com>
2022-02-19 16:06:35 +0330

    working

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,18 +1,40 @@
-import axios from 'axios';
+// import axios from 'axios';
+// const fetch = require("node-fetch");
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+// const SCRAPING_URL = "https://en.wikipedia.org/wiki/Corruption_Perceptions_Index";
 
+// export default function get_corruption_data(){
 
-export default function get_corruption_data(){
+//     fetch(SCRAPING_URL)
+//     .then(response => {
+//         "reposne is "+response;
+//     }).then(data => {
+//         return data;
+//     })
+//     // .catch(err => {
+//     //     return "error is "+err;
+//     // })
+// }
 
-    axios.get('https://en.wikipedia.org/wiki/Corruption_Perceptions_Index')
-    .then(response => {
-        const text = response.text();
-        const dom = new JSDOM(text);
-        return text;
-    })
-    .catch(err => {
-        return 'we got an error '+err;
-    })
-}
+const fetch = require("node-fetch");
+
+// function to get the raw data
+const getRawData = (URL) => {
+   return fetch(URL)
+      .then((response) => response.text())
+      .then((data) => {
+         return data;
+      });
+};
+
+// URL for data
+const URL = "https://en.wikipedia.org/wiki/Cricket_World_Cup";
+
+// start of the program
+const getCricketWorldCupsList = async () => {
+   const cricketWorldCupRawData = await getRawData(URL);
+   return cricketWorldCupRawData;
+};
+
+// invoking the main function
+export default getCricketWorldCupsList();
